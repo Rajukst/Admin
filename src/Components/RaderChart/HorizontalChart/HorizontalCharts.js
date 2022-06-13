7782866444417bcc88b493d69a5f38e12698de48
Rajukst <rajuahamedkst15@gmail.com>
2022-06-14 01:12:33 +0600

    apexchart added

--- src/Components/RaderChart/HorizontalChart/HorizontalCharts.js
+++ src/Components/RaderChart/HorizontalChart/HorizontalCharts.js
@@ -1,65 +1,60 @@
 import React from 'react';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
-  import { Bar } from 'react-chartjs-2';
-  import faker from 'faker';
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  );
-const HorizontalCharts = () => {
-    const options = {
-        indexAxis: 'y',
-        elements: {
-          bar: {
-            borderWidth: 2,
-          },
-        },
-        responsive: true,
-        plugins: {
+import ReactApexChart from "apexcharts"
+const myData = {
+          
+    series: [{
+      name: 'PRODUCT A',
+      data: [44, 55, 41, 67, 22, 43, 21, 49]
+    }, {
+      name: 'PRODUCT B',
+      data: [13, 23, 20, 8, 13, 27, 33, 12]
+    }, {
+      name: 'PRODUCT C',
+      data: [11, 17, 15, 15, 21, 14, 15, 13]
+    }],
+    options: {
+      chart: {
+        type: 'bar',
+        height: 350,
+        stacked: true,
+        stackType: '100%'
+      },
+      responsive: [{
+        breakpoint: 480,
+        options: {
           legend: {
-            position: 'right',
-          },
-          title: {
-            display: true,
-            text: 'Chart.js Horizontal Bar Chart',
-          },
-        },
-      };
-      const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-      const data = {
-        labels,
-        datasets: [
-          {
-            label: 'Dataset 1',
-            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          },
-          {
-            label: 'Dataset 2',
-            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-          },
+            position: 'bottom',
+            offsetX: -10,
+            offsetY: 0
+          }
+        }
+      }],
+      xaxis: {
+        categories: ['2011 Q1', '2011 Q2', '2011 Q3', '2011 Q4', '2012 Q1', '2012 Q2',
+          '2012 Q3', '2012 Q4'
         ],
-      }; 
+      },
+      fill: {
+        opacity: 1
+      },
+      legend: {
+        position: 'right',
+        offsetX: 0,
+        offsetY: 50
+      },
+    },
+  
+  
+  };
+const HorizontalCharts = () => {
+     
     return (
         <div>
-            <Bar options={options} data={data} />;
+     <ReactApexChart options={myData.options} series={myData.series} type="bar" height={350} />
         </div>
     );
 };
 
 export default HorizontalCharts;
+
+
